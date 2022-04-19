d9939eb3f66de29a21bbb584e556921bba8bfd59
dbobrzyk <d.bobrzyk@dook.pro>
2022-04-19 15:19:48 +0200

    #3 Create better view for ble device item, Add swipe to refresh

--- app/src/main/java/com/example/blet/MainActivity.kt
+++ app/src/main/java/com/example/blet/MainActivity.kt
@@ -11,28 +11,37 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.example.blet.ui.theme.BLETTheme
-import java.lang.Exception
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -40,55 +49,109 @@ import kotlinx.coroutines.flow.update
 
 class MainActivity : ComponentActivity() {
 
-    private val _viewState: MutableStateFlow<String> = MutableStateFlow(
-        String()
+    private val _viewState: MutableStateFlow<BleViewState> = MutableStateFlow(
+        BleViewState()
     )
-    val viewState: StateFlow<String> = _viewState.asStateFlow()
+    val viewState: StateFlow<BleViewState> = _viewState.asStateFlow()
 
     // Device scan callback.
     private val leScanCallback: ScanCallback = object : ScanCallback() {
         override fun onScanResult(callbackType: Int, result: ScanResult) {
-            if (!viewState.value.contains("Scanning results:")) {
-                _viewState.update { "Scanning results:" }
-            }
+
             super.onScanResult(callbackType, result)
             Log.d("BLE", "Scanning result: $result")
-            _viewState.update { currentState -> (currentState + "\n\n" + result) }
+            val newList = viewState.value.listOfBleDevices + result
+            _viewState.update { currentState ->
+                currentState.copy(
+                    viewHeader = "Scanning results:",
+                    listOfBleDevices = newList,
+                    dataLoading = false
+                )
+            }
         }
     }
     private var scanning = false
     private val handler = Handler()
     private val SCAN_PERIOD: Long = 10000
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _viewState.update { "Bluetooth Scanner" }
         setContent {
             BLETTheme {
-                val text = viewState.collectAsState()
+                val state = viewState.collectAsState()
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    LazyColumn(modifier = Modifier.padding(16.dp)) {
-                        item {
-                            Text(text.value)
-                        }
-                        item() {
-                            Text(text = "TRY AGAIN", modifier = Modifier
-                                .clickable {
-                                    scanLeDevice()
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Color.Gray
+                        ), color = MaterialTheme.colors.background
+                ) {
+                    SwipeRefresh(
+                        state = rememberSwipeRefreshState(state.value.dataLoading && scanning),
+                        onRefresh = { startScan() },
+                    ) {
+                        LazyColumn(modifier = Modifier.padding(16.dp)) {
+                            item {
+                                Text(
+                                    state.value.viewHeader,
+                                    style = TextStyle(fontWeight = FontWeight.Bold),
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(top = 48.dp, bottom = 16.dp)
+                                )
+                            }
+//                            if (state.value.dataLoading) {
+//                                item {
+//                                    Box(modifier = Modifier.fillMaxWidth()) {
+//                                        CircularProgressIndicator(Modifier.align(Alignment.Center))
+//                                    }
+//
+//                                }
+//                            }
+                            items(state.value.listOfBleDevices.size) { index ->
+                                Box(
+                                    modifier = Modifier
+                                        .padding(vertical = 8.dp)
+                                        .shadow(5.dp, RoundedCornerShape(10.dp))
+                                        .clip(RoundedCornerShape(10.dp))
+                                        .background(
+                                            Color.White
+                                        )
+                                        .fillMaxWidth()
+
+                                ) {
+                                    val item = state.value.listOfBleDevices[index]
+                                    Row {
+                                        Image(
+                                            painter = painterResource(id = R.drawable.ic_bluetooth),
+                                            contentDescription = null,
+                                            modifier = Modifier
+                                                .padding(16.dp)
+                                                .size(48.dp)
+                                                .align(Alignment.CenterVertically)
+                                        )
+                                        Text(
+                                            text = "Device id: ${item.device} \nDevice name: ${item.device.name}",
+                                            modifier = Modifier.align(Alignment.CenterVertically)
+                                        )
+                                    }
                                 }
-                                .padding(16.dp)
-                                .background(Color.Green)
-                                .fillMaxWidth()
-                            )
+
+                            }
 
                         }
-
                     }
                 }
             }
         }
 
+        startScan()
+    }
+
+    fun startScan() {
         if (!hasPermissions(this, permissions)) {
             Log.d("BLE", "No permission")
             requestPermissions(permissions.toTypedArray(), REQUEST_CODE)
@@ -102,13 +165,22 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-
     @SuppressLint("MissingPermission")
     private fun scanLeDevice() {
-        _viewState.update { "Scanning started..." }
+        _viewState.update { currentState ->
+            currentState.copy(
+                viewHeader = "Scanning...",
+                dataLoading = true,
+                listOfBleDevices = listOf()
+            )
+        }
         val bluetoothAdapter = (getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager)?.adapter
         val bluetoothLeScanner = bluetoothAdapter?.bluetoothLeScanner
-        if (!scanning && hasPermissions(this, permissions)) { // Stops scanning after a pre-defined scan period.
+        if (!scanning && hasPermissions(
+                this,
+                permissions
+            ) && bluetoothAdapter?.isEnabled == true
+        ) { // Stops scanning after a pre-defined scan period.
             Log.d("BLE", "Permissions OK - Start scanning")
             handler.postDelayed({
                 scanning = false
@@ -117,6 +189,13 @@ class MainActivity : ComponentActivity() {
             scanning = true
             bluetoothLeScanner?.startScan(leScanCallback)
         } else {
+            _viewState.update { currentState ->
+                currentState.copy(
+                    viewHeader = "Something went wrong, try again",
+                    dataLoading = false,
+                    listOfBleDevices = listOf()
+                )
+            }
             scanning = false
             bluetoothLeScanner?.stopScan(leScanCallback)
         }
@@ -134,6 +213,7 @@ class MainActivity : ComponentActivity() {
     companion object {
         val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             listOf(
+                Manifest.permission.BLUETOOTH_CONNECT,
                 Manifest.permission.BLUETOOTH_ADMIN,
                 Manifest.permission.BLUETOOTH,
                 Manifest.permission.ACCESS_FINE_LOCATION,
@@ -151,4 +231,11 @@ class MainActivity : ComponentActivity() {
         const val REQUEST_CODE = 175
     }
 }
+
+@Immutable
+data class BleViewState(
+    val viewHeader: String = "Bluetooth Devices Scanner",
+    val dataLoading: Boolean = false,
+    val listOfBleDevices: List<ScanResult> = listOf()
+)
 
