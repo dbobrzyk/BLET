d85e3859d2f640ba4079fb2a5ce00e29d378f378
dbobrzyk <d.bobrzyk@dook.pro>
2022-04-19 14:31:47 +0200

    #2 Add scanning and showing bluetooth devices

--- app/src/main/java/com/example/blet/MainActivity.kt
+++ app/src/main/java/com/example/blet/MainActivity.kt
@@ -1,40 +1,154 @@
 package com.example.blet
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.example.blet.ui.theme.BLETTheme
+import java.lang.Exception
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MainActivity : ComponentActivity() {
+
+    private val _viewState: MutableStateFlow<String> = MutableStateFlow(
+        String()
+    )
+    val viewState: StateFlow<String> = _viewState.asStateFlow()
+
+    // Device scan callback.
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            if (!viewState.value.contains("Scanning results:")) {
+                _viewState.update { "Scanning results:" }
+            }
+            super.onScanResult(callbackType, result)
+            Log.d("BLE", "Scanning result: $result")
+            _viewState.update { currentState -> (currentState + "\n\n" + result) }
+        }
+    }
+    private var scanning = false
+    private val handler = Handler()
+    private val SCAN_PERIOD: Long = 10000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _viewState.update { "Bluetooth Scanner" }
         setContent {
             BLETTheme {
+                val text = viewState.collectAsState()
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LazyColumn(modifier = Modifier.padding(16.dp)) {
+                        item {
+                            Text(text.value)
+                        }
+                        item() {
+                            Text(text = "TRY AGAIN", modifier = Modifier
+                                .clickable {
+                                    scanLeDevice()
+                                }
+                                .padding(16.dp)
+                                .background(Color.Green)
+                                .fillMaxWidth()
+                            )
+
+                        }
+
+                    }
                 }
             }
         }
+
+        if (!hasPermissions(this, permissions)) {
+            Log.d("BLE", "No permission")
+            requestPermissions(permissions.toTypedArray(), REQUEST_CODE)
+        } else {
+            try {
+                Log.d("BLE", "Prepare for scan")
+                scanLeDevice()
+            } catch (e: Exception) {
+                Log.d("BLE", "Scanning failed: \n\n $e")
+            }
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun scanLeDevice() {
+        _viewState.update { "Scanning started..." }
+        val bluetoothAdapter = (getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager)?.adapter
+        val bluetoothLeScanner = bluetoothAdapter?.bluetoothLeScanner
+        if (!scanning && hasPermissions(this, permissions)) { // Stops scanning after a pre-defined scan period.
+            Log.d("BLE", "Permissions OK - Start scanning")
+            handler.postDelayed({
+                scanning = false
+                bluetoothLeScanner?.stopScan(leScanCallback)
+            }, SCAN_PERIOD)
+            scanning = true
+            bluetoothLeScanner?.startScan(leScanCallback)
+        } else {
+            scanning = false
+            bluetoothLeScanner?.stopScan(leScanCallback)
+        }
+    }
+
+    private fun hasPermissions(context: Context, permissions: List<String>): Boolean {
+        for (perm in permissions) {
+            if (ActivityCompat.checkSelfPermission(context, perm) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    companion object {
+        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            listOf(
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.BLUETOOTH_SCAN
+            )
+        } else {
+            listOf(
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        }
+        const val REQUEST_CODE = 175
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BLETTheme {
-        Greeting("Android")
-    }
-}
